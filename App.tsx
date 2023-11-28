53511a26416f3d6ed5a2c37ecbfceb2aea41c2fe
CruzAmbrocio <cruzambrocio1409@gmail.com>
2023-11-27 18:47:51 -0600

    custom assets, and custom icons added

--- App.tsx
+++ App.tsx
@@ -24,6 +24,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import CustomIcon from './src/components/CustomIcon';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -72,6 +73,7 @@ function App(): JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
+        <CustomIcon name="search" size={50} />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
